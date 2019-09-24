cbc64d4b65ea54bbcec70fff30d6c70b2a3769cf
blueXstar597 <dannyou597@gmail.com>
2019-09-23 22:14:46 -0400

    added the frontend UICollection View and worked on the transition from screen 1 to screen 2

---
+++ coreMLSwipeControl/coreMLSwipeControl/CustomCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomCollectionViewController.swift
+//  coreMLSwipeControl
+//
+//  Created by Danny Ou on 2019-09-08.
+//  Copyright © 2019 Danny Ou. All rights reserved.
+//
+
+import Foundation
---
+++ coreMLSwipeControl/coreMLSwipeControl/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  coreMLSwipeControl
+//
+//  Created by Danny Ou on 2019-09-03.
+//  Copyright © 2019 Danny Ou. All rights reserved.
+//
+
+import Foundation
